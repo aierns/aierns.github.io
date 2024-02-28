9098be0aca4aca53c457db243cfcabdd5b939d80
aierns <dzx836918127@gmail.com>
2024-02-28 11:54:12 +0800

    Site updated: 2024-02-28 11:54:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/14/2024/","2024/02/14/Algorithm/","2024/02/16/AIGC/","2024/02/12/C/","2024/02/12/hello-world/","2024/02/14/Java/"];function toRandomPost(){
+var posts=["2024/02/14/2024/","2024/02/16/AIGC/","2024/02/12/C/","2024/02/14/Algorithm/","2024/02/28/ProgrammingOptimization/","2024/02/12/hello-world/","2024/02/14/Java/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
